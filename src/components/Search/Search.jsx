0df78643502ad09422f570c897f90827de8060b5
Max <max.book@list.ru>
2020-07-08 22:35:30 +0500

    Fix interface

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,9 +5,6 @@ const Search = (props) => {
     return (
         <div className={style.search}>
             <input className={style.input_city} type="text" placeholder="Название города"/>
-            <button className={style.btn}>
-                <span className="material-icons">search</span>
-            </button>
         </div>
     )
 }
